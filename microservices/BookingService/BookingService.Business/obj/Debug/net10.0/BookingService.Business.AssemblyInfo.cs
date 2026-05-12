0591d60c8404ec748377e718d6257fdeea0425b2
K4v4Try4 <cavatria005@gmail.com>
2026-05-12 09:17:29 +0200

    Aggiornamento del file nuget config e nuget publish yml

--- microservices/BookingService/BookingService.Business/obj/Debug/net10.0/BookingService.Business.AssemblyInfo.cs
+++ microservices/BookingService/BookingService.Business/obj/Debug/net10.0/BookingService.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingService.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13da813c0529438faff547981efbf08ba5376716")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+757042dff37ca2a0fa815ddad27ba66d0cffbc77")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingService.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingService.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
